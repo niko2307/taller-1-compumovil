1f76edc51a0652a7cefd200cfbe0d38a58f9a70c
Nicolas Aguilar <aguilar-nicolas@javeriana.edu.co>
2024-02-21 19:00:48 -0500

    este-es-para-los-permisos-del-manifest

--- app/src/main/java/com/example/taller1/MainActivity.kt
+++ app/src/main/java/com/example/taller1/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.taller1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
+    var arreglo : MutableList<String> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
 
+
+
+
     }
+
 }
